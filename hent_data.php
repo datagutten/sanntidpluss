7f22c39c4b4183597d6fb5d37157dc5f99be9cd3
datagutten <datagutten@datagutten.net>
2015-02-15 20:03:33 +0100

    Code cleanup

--- hent_data.php
+++ hent_data.php
@@ -1,14 +1,13 @@
 <?Php
-require '../trafikanten/ruter_rest_class.php';
-$ruter=new ruter_rest;
-$coordinates='(x=600810,y=6631230)';
-$proposals=1;
+require 'sanntidpluss_class.php';
+$sanntidpluss=new sanntidpluss;
+
+$proposals=5;
 $maxdistrance=1400;
 if(isset($_GET['X']) && isset($_GET['Y']))
-	$uri="Place/GetClosestStops/?coordinates=(X=".$_GET["X"].",Y=".$_GET["Y"].")&proposals=5"; 
+	$uri="Place/GetClosestStops/?coordinates=(X=".$_GET["X"].",Y=".$_GET["Y"].")&proposals=$proposals"; 
 elseif(isset($_GET['navn']))
 	$uri='Place/GetPlaces/'.urlencode(utf8_decode($_GET['navn']));
 
-//print_r($ruter->get('Place/GetClosestStops?coordinates=$coordinates&proposals=$proposals&maxdistance=$maxdistance'));
-echo $json=$ruter->get($uri,true);
-//print_r(json_decode($json,true));
+echo $json=$sanntidpluss->get($uri,true);
+?>
